f8b6b6221d0174f289b017220a6154651e81a953
L2Control <pannes.pannes@gmail.com>
2020-10-22 11:44:09 +0200

    Updated version

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  progressBar();
+};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.querySelector(".header-progress").value = scrolled;
+}
